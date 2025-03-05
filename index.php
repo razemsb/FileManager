0081cdafd474409b2033b6b2aebb78d8a757db84
raze <155824568+razemsb@users.noreply.github.com>
2025-03-05 23:39:51 +0300

    update index.php

--- index.php
+++ index.php
@@ -484,20 +484,5 @@ $folders = getFolders(__DIR__);
                 </div>
             </div>
     </main>
-<script>
-/*
-  function dragstartHandler(ev) {
-    // Add the target element's id to the data transfer object
-    ev.dataTransfer.setData("text/plain", ev.target.id);
-  }
-
-  window.addEventListener("DOMContentLoaded", () => {
-    // Get the element by id
-    const element = document.getElementById("dragcart");
-    // Add the ondragstart event listener
-    element.addEventListener("dragstart", dragstartHandler);
-  });
-*/
-</script>
 </body>
 </html>
